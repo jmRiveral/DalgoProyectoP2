0e19d89aa44c5d114ac9477b1b9893d73cd85756
Luimarco98 <lds.carrascal10@uniandes.edu.co>
2022-11-18 17:58:45 -0500

    Add files via upload

--- Graph.java
+++ Graph.java
@@ -6,11 +6,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
-import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.io.InputStreamReader;
-
 class Graph{
     
    
@@ -22,10 +17,10 @@ class Graph{
         V=v; // un arreglo de vertices
         elem=paramElem; //un elemento de ese arreglo
         for(int i=0;i< v.size()-1;i++){
-            if(!(v.get(i).equals(v.get(i+1)))){
+            
                 
                 adjVertices.put(v.get(i), v);
-            }
+            
         }
         
         
@@ -73,32 +68,6 @@ class Graph{
         
         return tp;
     }
-
-
-public class Graph{
-    public static void main(String[]args) throws Exception{
-        Graph instancia = new Graph();
-        try(InputStreamReader is = new InputStreamReader(System.in); BufferedReader br = new BufferedReader(is);){
-            String line = br.readLine();
-            int casos = Integer.parseInt(line);
-            line = br.readLine();
-            final ArrayList palabras = new ArrayList();
-            for (int i =0; i<casos && line != null && line.length()>0;i++){
-                palabras.clear();
-                String enteros = br.readLine();
-                int n = Integer.parseInt(enteros.substring(0,0));
-                int m = Integer.parseInt(enteros.substring(1,1));
-                line = br.readLine();
-                for (int j =0; j<n && line != null && line.length()>0;j++){
-                    line= line.substring(1);
-                    final ArrayList listalinea = new ArrayList();
-                    line = br.readLine();
-            }
-
-
-        }
-    }
-}
     
     
     
